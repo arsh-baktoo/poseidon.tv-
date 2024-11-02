271a7f02fe145ad6ece5536375e85d5a2265f0e6
arshbaktoo <81901192+arsh-baktoo@users.noreply.github.com>
2024-11-02 20:16:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.continue-btn').addEventListener('click', function() {
+    document.querySelector('.popup-overlay').style.display = 'none';
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+    document.querySelector('.popup-overlay').style.display = 'none';
+});
